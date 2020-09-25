f88720ef80e3a7f7098806992d2a1abc4c889386
Magrishb <bmagrish@gmail.com>
2020-09-24 20:01:19 -0500

    Set my questions in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,59 @@
-// array of questions for user
-// const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// ];
+// array of questions for user
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Title of the Project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe the Project:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the installation process?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How is the project used?",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What is the license?",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who's contributed to the projet?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Test the project?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Provide your email address?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github pages?",
+    },
+    
+    
+ ]
+
 
 // function to write README file
 // function writeToFile(fileName, data) {
@@ -16,6 +68,3 @@
 //init();
 
 
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown.js");
