dd483f069fe6d2443542fccb2fb6d374fb93904f
Magrishb <bmagrish@gmail.com>
2020-09-24 20:33:59 -0500

    Finished MarkDown.js to framework

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${response.title}
 
+  ## Description
+  ${response.description}
+
+  ## Installation
+  ${response.installation}
+
+  ## Usage
+  ${response.usage}
+
+  ## License
+  ${response.license}
+
+  ## Contributing
+  ${response.contributing}
+
+  ## Testing
+  ${response.test}
+
+  ## Questions
+  -[GitHub](https://github.com/${response.github})
+  -[Email](${response.email})
 `;
 }
 
